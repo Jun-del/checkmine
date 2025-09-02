c7b1335074911ca08d344714330afad1ce8ddd0e
Jun-del <78585106+Jun-del@users.noreply.github.com>
2025-09-02 19:05:44 +0800

    feat(server): add basic server and health check route

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, http::StatusCode, routing::get};
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health", get(StatusCode::OK));
+
+    // run our app with hyper, listening globally on port 3000
+    let addr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    tracing::info!("listening on http://{}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
